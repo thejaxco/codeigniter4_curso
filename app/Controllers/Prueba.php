5d0e4d41a01f1318a59c1dd2e7da2b203db92fbd
thejaxco <thejaxco@gmail.com>
2019-10-29 11:30:42 +0000

    Primer Commit

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,22 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+		echo "Esta es la función Index..";
+	}
+
+	//--------------------------------------------------------------------
+
+public function test()
+{
+  echo " Esto es la función test...";
+}
+
+public function test2()
+{
+  echo " Esto es la función test2...";
+}
+
+}
